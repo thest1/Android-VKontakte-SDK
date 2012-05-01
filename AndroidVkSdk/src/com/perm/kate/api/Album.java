219be4fc68e3f44fca54e75320a03270169b7946
Fedor Vlasov <thest2@gmail.com>
2012-05-01 18:29:56 +0600

    Parse comment_privacy in album

--- AndroidVkSdk/src/com/perm/kate/api/Album.java
+++ AndroidVkSdk/src/com/perm/kate/api/Album.java
@@ -13,6 +13,7 @@ public class Album {
     public long updated;
     public long size;
     public long privacy;
+    public long comment_privacy;
 
     public static Album parse(JSONObject o) throws JSONException {
         Album a = new Album();
@@ -22,9 +23,12 @@ public class Album {
         a.description = Api.unescape(o.optString("description"));
         a.thumb_id = Long.parseLong(o.optString("thumb_id"));
         a.created = Long.parseLong(o.optString("created"));
-        String privacy=o.optString("privacy");
-        if(privacy!=null && !privacy.equals(""))
+        String privacy = o.optString("privacy");
+        if(privacy != null && !privacy.equals(""))
             a.privacy = Long.parseLong(privacy);
+        String comment_privacy = o.optString("comment_privacy");
+        if(comment_privacy != null && !comment_privacy.equals(""))
+            a.comment_privacy = Long.parseLong(comment_privacy);
         a.size = Long.parseLong(o.optString("size"));
         a.updated = Long.parseLong(o.optString("updated"));
         return a;
